fbc9ec894d39fbd12f844d9a26ffffceb4aab8a2
Francesco Lodolo <flod@lodolo.net>
2020-12-22 11:21:44 +0100

    Move from Travis to GitHub Actions, use Black (#29)
    * Move from Travis to GitHub Actions
    * Format code with Black

--- tests/test_string_projectconfig.py
+++ tests/test_string_projectconfig.py
@@ -11,24 +11,23 @@ from six import iteritems
 
 
 class TestStringExtraction(unittest.TestCase):
-
     def setUp(self):
-        self.testfiles_path = os.path.join(
-            os.path.dirname(__file__), 'testfiles')
-        self.storage_path = os.path.join(self.testfiles_path, 'output_android')
+        self.testfiles_path = os.path.join(os.path.dirname(__file__), "testfiles")
+        self.storage_path = os.path.join(self.testfiles_path, "output_android")
 
     def testGetAndroidStringsGerman(self):
-        toml_path = os.path.join(self.testfiles_path, 'android', 'l10n.toml')
+        toml_path = os.path.join(self.testfiles_path, "android", "l10n.toml")
         extraction = tmx_products.tmx_projectconfig.StringExtraction(
-            toml_path, self.storage_path, 'en-US', 'test')
+            toml_path, self.storage_path, "en-US", "test"
+        )
         extraction.extractStrings()
 
         strings_locale = extraction.translations
         self.assertEqual(len(strings_locale), 11)
-        self.assertEqual(len(strings_locale['it']), 4)
-        self.assertEqual(len(strings_locale['en-US']), 17)
-        self.assertEqual(len(strings_locale['es-ES']), 5)
+        self.assertEqual(len(strings_locale["it"]), 4)
+        self.assertEqual(len(strings_locale["en-US"]), 17)
+        self.assertEqual(len(strings_locale["es-ES"]), 5)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
