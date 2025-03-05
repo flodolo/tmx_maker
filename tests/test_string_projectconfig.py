8f0881994a3c3b4b38343681b5b14600178fb66f
Francesco Lodolo <flod@lodolo.net>
2025-03-05 13:08:49 +0100

    Use moz.l10n to parse TOML config files (#34)

--- tests/test_string_projectconfig.py
+++ tests/test_string_projectconfig.py
@@ -11,10 +11,10 @@ class TestStringExtraction(unittest.TestCase):
         self.testfiles_path = os.path.join(os.path.dirname(__file__), "testfiles")
         self.storage_path = os.path.join(self.testfiles_path, "output_android")
 
-    def testGetAndroidStringsGerman(self):
+    def testGetAndroidStrings(self):
         toml_path = os.path.join(self.testfiles_path, "android", "l10n.toml")
         extraction = tmx_products.tmx_projectconfig.StringExtraction(
-            toml_path, self.storage_path, "en-US", "test"
+            toml_path, self.storage_path, "en-US", "test", True
         )
         extraction.extractStrings()
 
@@ -23,6 +23,20 @@ class TestStringExtraction(unittest.TestCase):
         self.assertEqual(len(strings_locale["it"]), 4)
         self.assertEqual(len(strings_locale["en-US"]), 17)
         self.assertEqual(len(strings_locale["es-ES"]), 5)
+
+    def testGetProductStrings(self):
+        toml_path = os.path.join(self.testfiles_path, "toml", "l10n.toml")
+        extraction = tmx_products.tmx_projectconfig.StringExtraction(
+            toml_path, self.storage_path, "en", "test", False
+        )
+        extraction.extractStrings()
+
+        strings_locale = extraction.translations
+        self.assertEqual(len(strings_locale), 4)
+        self.assertEqual(len(strings_locale["it"]), 1)
+        self.assertEqual(len(strings_locale["en"]), 2)
+        self.assertEqual(len(strings_locale["de"]), 2)
+        self.assertEqual(len(strings_locale["fr"]), 0)
 
 
 if __name__ == "__main__":
