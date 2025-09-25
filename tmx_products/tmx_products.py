21eb73b522d94b038e2df2041abd3194493bd878
Francesco Lodolo <flod@lodolo.net>
2025-09-25 13:48:05 +0200

    Refactor code

--- tmx_products/tmx_products.py
+++ tmx_products/tmx_products.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
-from compare_locales import parser
-from configparser import ConfigParser
+from tmx_products.functions import get_cli_parameters, get_config
 from moz.l10n.resource import parse_resource
 from moz.l10n.message import serialize_message
 from moz.l10n.model import Entry
-import argparse
 import codecs
 import json
 import os
@@ -190,76 +188,8 @@ class StringExtraction:
 
 
 def main():
-    # Read command line input parameters
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--path",
-        dest="repo_path",
-        help="Path to locale files",
-        required=True,
-    )
-    parser.add_argument(
-        "--locale",
-        dest="locale_code",
-        help="Locale code",
-        required=True,
-    )
-    parser.add_argument(
-        "--ref",
-        dest="reference_code",
-        help="Reference locale code",
-        required=True,
-    )
-    parser.add_argument(
-        "--repo", dest="repository_name", help="Repository name", required=True
-    )
-    parser.add_argument(
-        "--append",
-        dest="append_mode",
-        action="store_true",
-        help="If set to 'append', translations will be added to an existing cache file",
-    )
-    parser.add_argument(
-        "--prefix",
-        dest="storage_prefix",
-        nargs="?",
-        help="This prefix will be prependended to the identified "
-        "path in string IDs (e.g. extensions/irc for Chatzilla)",
-        default="",
-    )
-    parser.add_argument(
-        "--output",
-        nargs="?",
-        type=str,
-        choices=["json", "php"],
-        help="Store only one type of output.",
-        default="",
-    )
-    args = parser.parse_args()
-
-    # Get absolute path of ../../config from the current script location (not the
-    # current folder)
-    config_folder = os.path.abspath(
-        os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "config")
-    )
-    # Read Transvision's configuration file from ../../config/config.ini
-    # If not available use a default storage folder to store data
-    config_file = os.path.join(config_folder, "config.ini")
-    if not os.path.isfile(config_file):
-        print(
-            "Configuration file /app/config/config.ini is missing. "
-            "Default settings will be used."
-        )
-        root_folder = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), os.pardir)
-        )
-        storage_path = os.path.join(root_folder, "TMX")
-        os.makedirs(storage_path, exist_ok=True)
-    else:
-        config_parser = ConfigParser()
-        config_parser.read(config_file)
-        storage_path = os.path.join(config_parser.get("config", "root"), "TMX")
-
+    args = get_cli_parameters()
+    storage_path = get_config()
     extracted_strings = StringExtraction(
         storage_path, args.locale_code, args.reference_code, args.repository_name
     )
--- tmx_products/tmx_projectconfig.py
+++ tmx_products/tmx_projectconfig.py
@@ -1,36 +1,11 @@
 #!/usr/bin/env python
 
 from compare_locales import parser
-from configparser import ConfigParser
+from tmx_products.functions import get_cli_parameters, get_config
 from moz.l10n.paths import L10nConfigPaths, get_android_locale
-import argparse
 import codecs
 import json
-import logging
 import os
-
-logging.basicConfig()
-# Get absolute path of ../../config from the current script location (not the
-# current folder)
-config_folder = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "config")
-)
-
-# Read Transvision's configuration file from ../../config/config.ini
-# If not available use a default storage folder to store data
-config_file = os.path.join(config_folder, "config.ini")
-if not os.path.isfile(config_file):
-    print(
-        "Configuration file /app/config/config.ini is missing. "
-        "Default settings will be used."
-    )
-    root_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-    storage_path = os.path.join(root_folder, "TMX")
-    os.makedirs(storage_path, exist_ok=True)
-else:
-    config_parser = ConfigParser()
-    config_parser.read(config_file)
-    storage_path = os.path.join(config_parser.get("config", "root"), "TMX")
 
 
 class StringExtraction:
@@ -234,49 +209,8 @@ class StringExtraction:
 
 
 def main():
-    # Read command line input parameters
-    parser = argparse.ArgumentParser()
-    parser.add_argument("toml_path", help="Path to root l10n.toml file")
-    parser.add_argument(
-        "--ref",
-        dest="reference_code",
-        help="Reference language code",
-        required=True,
-    )
-    parser.add_argument(
-        "--repo", dest="repository_name", help="Repository name", required=True
-    )
-    parser.add_argument(
-        "--append",
-        dest="append_mode",
-        action="store_true",
-        help="If set to 'append', translations will be added to an existing cache file",
-    )
-    parser.add_argument(
-        "--android",
-        dest="android_project",
-        action="store_true",
-        help="If passed, the script will parse the config file using Android locale codes",
-        default=False,
-    )
-    parser.add_argument(
-        "--prefix",
-        dest="storage_prefix",
-        nargs="?",
-        help="This prefix will be prependended to the identified "
-        "path in string IDs (e.g. extensions/irc for Chatzilla)",
-        default="",
-    )
-    parser.add_argument(
-        "--output",
-        nargs="?",
-        type=str,
-        choices=["json", "php"],
-        help="Store only one type of output.",
-        default="",
-    )
-    args = parser.parse_args()
-
+    args = get_cli_parameters(config=True)
+    storage_path = get_config()
     extracted_strings = StringExtraction(
         args.toml_path,
         storage_path,
